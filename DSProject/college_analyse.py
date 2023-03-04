8e30734f5005e318aee85eaab6669509f8e18ee6
Cris Wang <184596413@qq.com>
2023-03-04 21:59:56 +0800

    some changes to code style

--- DSProject/college_analyse.py
+++ DSProject/college_analyse.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import jieba
 from pyecharts.charts import Bar, Pie, Geo, Liquid
 from pyecharts.commons.utils import JsCode
 from pyecharts import options as opts
 from pyecharts.globals import ChartType, CurrentConfig, NotebookType
+from wordcloud import WordCloud, ImageColorGenerator
+from matplotlib import pyplot as plt
 
 CurrentConfig.NOTEBOOK_TYPE = NotebookType.JUPYTER_LAB
 
@@ -201,3 +204,64 @@ pie.set_global_opts(
         )
 pie.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}: {d}%"))
 pie.render("中国高校属性分析饼状图.html")
+
+# 绘制中国高校校名词云图
+def create_wordcloud(df):
+    # 分词
+    text = ''
+    for line in df['name']:
+        text += ' '.join(jieba.cut(line, cut_all=False))
+        text += ' '
+    backgroud_Image = plt.imread('rocket.jpg')
+
+    wc = WordCloud(
+        background_color='white',
+        mask=backgroud_Image,
+        font_path='font_demo.ttf',
+        max_words=1000,
+        max_font_size=150,
+        min_font_size=15,
+        prefer_horizontal=1,
+        random_state=50,
+    )
+    wc.generate_from_text(text)
+    img_colors = ImageColorGenerator(backgroud_Image)
+    wc.recolor(color_func=img_colors)
+    # 看看词频高的有哪些
+    process_word = WordCloud.process_text(wc, text)
+    sort = sorted(process_word.items(), key=lambda e: e[1], reverse=True)
+    print(sort[:50])
+    plt.imshow(wc)
+    plt.axis('off')
+    wc.to_file("中国校名词云.jpg")
+
+
+df = pd.read_csv("college_data.csv")
+
+# 去除大学
+df_name = df['name'].str.replace('大学', '')
+dict_name = {'name': df_name.values, 'numbers': df_name.index}
+df_name = pd.DataFrame(dict_name)
+
+# 去除学院
+df_name = df_name['name'].str.replace('学院', '')
+dict_name = {'name': df_name.values, 'numbers': df_name.index}
+df_name = pd.DataFrame(dict_name)
+
+# 去除专科学校
+df_name = df_name['name'].str.replace('专科学校', '')
+dict_name = {'name': df_name.values, 'numbers': df_name.index}
+df_name = pd.DataFrame(dict_name)
+
+df = pd.read_csv("college_data.csv")
+df_new = df.drop_duplicates(subset=['name'])
+df_site = df_new[df_new['site'] != '——']
+df_site = df_site[df_site['site'] != '------']
+site_counts = df_site['site'].value_counts()
+dict_site = {'name': site_counts.index, 'counts': site_counts.values}
+for city in site_counts.index:
+    df_name = df_name['name'].str.replace(city, '')
+    dict_name = {'name': df_name.values, 'numbers': df_name.index}
+    df_name = pd.DataFrame(dict_name)
+
+create_wordcloud(df_name)
